ad4d01578c4dffe65de230429c35ce447953dcd9
Jess purvis <jess.purvis@and.digital>
2023-02-01 16:05:38 +0000

    matching the slides on ex 4

--- App.js
+++ App.js
@@ -1,54 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { View, Text, Image, ScrollView } from 'react-native';
 import { getPopularMovies } from './src/api/movies';
 
 export default function App() {
   const [popularMovies, setPopularMovies] = useState([]);
 
-  useEffect(() => {
-    fetchPopularMovies();
-  }, []);
+	useEffect(() => {
+		fetchPopularMovies();
+	}, []);
+	
+	const fetchPopularMovies = async () => {
+		const fetchedPopularMovies = await getPopularMovies();
+		setPopularMovies(fetchedPopularMovies);
+	}
 
-  const fetchPopularMovies = async () => {
-    const fetchedPopularMovies = await getPopularMovies();
-    setPopularMovies(fetchedPopularMovies);
-  };
+	const MovieCard = ({ image, title }) => (
+		<View>
+			<Image source={{ uri: image }} style={{ width: 200, height: 350, marginRight: 8 }} />
+      <Text>{title}</Text>
+    </View>
+	)
 
   return (
-    <View style={styles.container}>
+    <View>
       <Text>MOST POPULAR MOVIES</Text>
-      <ScrollView horizontal>
-        {popularMovies.map(popularMovie => (
+	    <ScrollView horizontal={true}>
+      {popularMovies.map(popularMovie => (
           <MovieCard
             key={popularMovie.title}
             image={popularMovie.posterImage}
             title={popularMovie.title}
           />
         ))}
-      </ScrollView>
-      <StatusBar style="auto" />
+	    </ScrollView>
     </View>
-  );
+	);
 }
-
-const MovieCard = ({title, image}) => (
-  <View>
-    <Image
-      source={{
-        uri: image,
-      }}
-      style={{width: 200, height: 350, marginRight: 8}}
-    />
-    <Text>{title}</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
