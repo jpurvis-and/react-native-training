a0b2d566199cd8036cacd1f728f4b969efe6a1da
Jess purvis <jess.purvis@and.digital>
2023-02-01 15:44:27 +0000

    updated to match slides

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
@@ -9,6 +9,7 @@ export default function App() {
     </View>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
