4f1216c4f8e9012abd1658fa5c2e26e8c0d6b817
Jess purvis <jess.purvis@and.digital>
2023-02-01 15:58:22 +0000

    matching slides

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-
-const URI =
-  'https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg';
-
+import React from 'react';
+import { View, Text, Image, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>MOST POPULAR MOVIES</Text>
-      <ScrollView horizontal>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </ScrollView>
-      <StatusBar style="auto" />
+	const MovieCard = ({ image, title }) => (
+		<View>
+			<Image source={{ uri: image }} style={{ width: 200, height: 350, marginRight: 8 }} />
+      <Text>{title}</Text>
     </View>
-  );
+	)
+
+  return (
+    <View>
+      <Text>MOST POPULAR MOVIES</Text>
+	    <ScrollView horizontal={true}>
+				<MovieCard image="https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" title="Sing 2"/>
+				<MovieCard image="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg" title="Spiderman No Way Home"/>
+				<MovieCard image="https://m.media-amazon.com/images/M/MV5BMzQ5ZDZhZDItZTNmZi00MWQ0LWJlNDUtZTE4ZWJmODNlM2Y3XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg" title="West Side Story"/>
+				<MovieCard image="https://m.media-amazon.com/images/M/MV5BYzdlMTMyZWQtZWNmMC00MTJiLWIyMWMtM2ZlZDdlYzZhNTc0XkEyXkFqcGdeQXVyMTMzNDE5NDM2._V1_FMjpg_UX1000_.jpg" title="House of Gucci"/>
+	    </ScrollView>
+    </View>
+	);
 }
-
-const MovieCard = () => (
-  <View>
-    <Image
-      source={{
-        uri: URI,
-      }}
-      style={{width: 200, height: 350, marginRight: 8}}
-    />
-    <Text>Sing 2</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
